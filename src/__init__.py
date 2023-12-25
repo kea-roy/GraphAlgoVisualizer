59711f2e78bdf71cec22692f3bfcb4c32f325b3b
kea-roy <146872846+kea-roy@users.noreply.github.com>
2023-12-25 15:47:20 +0800

    Improved UI and UX. Added fields to input source and sink nodes and set up all inputs for different algorithms. Added run button that checks preconditions.

--- src/__init__.py
+++ src/__init__.py
@@ -10,31 +10,24 @@ from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 
 matplotlib.use("TkAgg")
 
-
-# Function that takes in name of algorithm and adjusts requirements
-def on_algo_selection_change(algoName):
-    print(algoName)
-
-
 # Create Graph
-
 graph = nx.DiGraph()
-graph.add_nodes_from('ABCDEFGH')
-graph.add_edges_from([
-    ('A', 'B', {'capacity': 4, 'flow': 0}),
-    ('A', 'C', {'capacity': 5, 'flow': 0}),
-    ('A', 'D', {'capacity': 7, 'flow': 0}),
-    ('B', 'E', {'capacity': 7, 'flow': 0}),
-    ('C', 'E', {'capacity': 6, 'flow': 0}),
-    ('C', 'F', {'capacity': 4, 'flow': 0}),
-    ('C', 'G', {'capacity': 1, 'flow': 0}),
-    ('D', 'F', {'capacity': 8, 'flow': 0}),
-    ('D', 'G', {'capacity': 1, 'flow': 0}),
-    ('E', 'H', {'capacity': 7, 'flow': 0}),
-    ('F', 'H', {'capacity': 6, 'flow': 0}),
-    ('G', 'H', {'capacity': 4, 'flow': 0}),
-
-])
+# graph.add_nodes_from('ABCDEFGH')
+# graph.add_edges_from([
+#     ('A', 'B', {'capacity': 4, 'flow': 0}),
+#     ('A', 'C', {'capacity': 5, 'flow': 0}),
+#     ('A', 'D', {'capacity': 7, 'flow': 0}),
+#     ('B', 'E', {'capacity': 7, 'flow': 0}),
+#     ('C', 'E', {'capacity': 6, 'flow': 0}),
+#     ('C', 'F', {'capacity': 4, 'flow': 0}),
+#     ('C', 'G', {'capacity': 1, 'flow': 0}),
+#     ('D', 'F', {'capacity': 8, 'flow': 0}),
+#     ('D', 'G', {'capacity': 1, 'flow': 0}),
+#     ('E', 'H', {'capacity': 7, 'flow': 0}),
+#     ('F', 'H', {'capacity': 6, 'flow': 0}),
+#     ('G', 'H', {'capacity': 4, 'flow': 0}),
+#
+# ])
 
 layout = {
     'A': [0, 1], 'B': [1, 2], 'C': [1, 1], 'D': [1, 0],
@@ -42,52 +35,13 @@ layout = {
 }
 
 
-# layout = nx.spring_layout(graph,seed=1)
-def update_graph(graph):
-    f = plt.Figure(figsize=(5, 5), dpi=100)
-    a = f.add_subplot(111)
-    # a.set_facecolor("w")
-    labelMap = {}
-    colorList = []
-
-    for u, v, e in graph.edges(data=True):
-        label = '{}/{}'.format(e['flow'], e['capacity'])
-        color = 'green' if e['flow'] < e['capacity'] else 'red'
-        labelMap[(u, v)] = label
-        colorList.append(color)
-
-    print(labelMap)
-    print(colorList)
-
-    nx.draw_networkx_edges(graph, layout, edge_color=colorList, ax=a, node_size=600)
-    nx.draw_networkx_edge_labels(graph, layout, edge_labels=labelMap, label_pos=0.7, ax=a)
-    nx.draw_networkx_nodes(graph, layout, node_color='steelblue', node_size=600, ax=a)
-    nx.draw_networkx_labels(graph, layout, ax=a)
-
-    # create matplotlib canvas using figure `f` and assign to widget `window`
-    canvas = FigureCanvasTkAgg(f, window)
-
-    # get canvas as tkinter's widget and `grid` in widget `window`
-    canvas.get_tk_widget().grid(row=0, column=0, rowspan=1)
+def backPressed():
+    print("back")
 
 
-# Create Window
-window = Tk()
-window.title("Window")
+def nextPressed():
+    print("next")
 
-# Create Frame
-widget_frame = Frame(window)
-widget_frame.grid(row=0, column=1, sticky=N)
-
-# Create Widgets
-# Label
-Label(widget_frame, text="Control Panel").grid(row=0, column=0, sticky=EW)
-# InputField
-inputField = Entry(widget_frame, justify='center')
-inputField.grid(row=1, column=0)
-
-
-# Buttons
 
 def select_file():
     filetypes = (
@@ -102,69 +56,225 @@ def select_file():
     return file
 
 
+def add_edge():
+    inp = edgeInputField.get().strip()
+    words = inp.split(" ")
+    if len(words) == 2:
+        graph.add_edge(words[0], words[1])
+    elif len(words) > 2:
+        new_str = inp.split(" ", 2)[-1].replace("'", '"')
+        try:
+            attributes = json.loads(new_str)
+            graph.add_edge(words[0], words[1], **attributes)
+        except json.decoder.JSONDecodeError as error:
+            print("ERROR: Couldn't parse input as JSON", type(error).__name__, "–", error,
+                  file=sys.stderr)
+            return
+    update_graph(graph)
+
+
+# Function that takes in name of algorithm and adjusts requirements
+def on_algo_selection_change(algoName):
+    if algoName in ["DFS", "BFS"]:
+        # Need a Directed/Undirected Graph and Start Node
+        print("Start Node")
+        startNodeLabel.grid(row=0, column=0)
+        startNodeField.grid(row=0, column=1)
+        sinkNodeLabel.grid_remove()
+        sinkNodeField.grid_remove()
+    elif algoName == "Dijkstra's":
+        # Need a Directed/Undirected Graph with weights and Start Node
+        print("Start Node")
+        startNodeLabel.grid(row=0, column=0)
+        startNodeField.grid(row=0, column=1)
+        sinkNodeLabel.grid_remove()
+        sinkNodeField.grid_remove()
+    elif algoName in ["Prim's", "Kruskal's"]:
+        # Need an Undirected Graph
+        print("None")
+        startNodeLabel.grid_remove()
+        startNodeField.grid_remove()
+        sinkNodeLabel.grid_remove()
+        sinkNodeField.grid_remove()
+    elif algoName == "Ford-Fulkerson":
+        # Need a Directed Graph with Capacities and Flows and Start Node and End Node
+        print("Source Node and Sink Node")
+        startNodeLabel.grid(row=0, column=0)
+        startNodeField.grid(row=0, column=1)
+        sinkNodeLabel.grid(row=1, column=0)
+        sinkNodeField.grid(row=1, column=1)
+
+
+# layout = nx.spring_layout(graph,seed=1)
+def update_graph(new_graph):
+    f = plt.Figure(figsize=(5, 5), dpi=100)
+    a = f.add_subplot(111)
+    # a.set_facecolor("w")
+    labelMap = {}
+    colorList = []
+
+    for u, v, e in new_graph.edges(data=True):
+        label = '{}/{}'.format(e['flow'], e['capacity'])
+        color = 'green' if e['flow'] < e['capacity'] else 'red'
+        labelMap[(u, v)] = label
+        colorList.append(color)
+
+    print(labelMap)
+    print(colorList)
+
+    nx.draw_networkx_edges(new_graph, layout, edge_color=colorList, ax=a, node_size=600)
+    nx.draw_networkx_edge_labels(new_graph, layout, edge_labels=labelMap, label_pos=0.7, ax=a)
+    nx.draw_networkx_nodes(new_graph, layout, node_color='steelblue', node_size=600, ax=a)
+    nx.draw_networkx_labels(new_graph, layout, ax=a)
+
+    # create matplotlib canvas using figure `f` and assign to widget `window`
+    canvas = FigureCanvasTkAgg(f, window)
+
+    # get canvas as tkinter's widget and `grid` in widget `window`
+    canvas.get_tk_widget().grid(row=0, column=0, rowspan=1)
+    global graph
+    graph = new_graph
+
+
 def import_graph():
+    graph_type = None
+    node_type = None
+    edge_type = None
+    data_tuple = True
+    algoName = selectedAlgo.get()
+    if algoName == "DFS" or algoName == "BFS":
+        print("Directed/Undirected")
+        graph_type = nx.Graph
+    elif algoName == "Dijkstra's":
+        print("Directed/Undirected Weighted")
+        graph_type = nx.Graph
+        data_tuple = (('weight', float),)
+    elif algoName in ["Prim's", "Kruskal's"]:
+        print("Undirected Weighted")
+        graph_type = nx.Graph
+        data_tuple = (('weight', float),)
+    elif algoName == "Ford-Fulkerson":
+        print("Directed Capacity Flow")
+        graph_type = nx.DiGraph
+        data_tuple = (('capacity', int), ('flow', int),)
+    else:
+        print("ERROR: Step 1 Not Completed", file=sys.stderr)
+        return
     file = select_file()
     if file is None:
         print("No file was selected")
         return
     try:
-        new_graph = nx.read_edgelist(file, create_using=nx.DiGraph, nodetype=str)
+        new_graph = nx.read_edgelist(file, create_using=graph_type, nodetype=node_type,
+                                     edgetype=edge_type)
+        # check required data is all present
+        for u, v, e in new_graph.edges(data=True):
+            for itemName, className in data_tuple:
+                if itemName in e:
+                    print(type(e[itemName]), className, isinstance(e[itemName], className))
+                if itemName not in e or not isinstance(e[itemName], className):
+                    print("Invalid File:", itemName, "data is not provided for edge", u, "-", v,
+                          file=sys.stderr)
+                    return
     except TypeError as error:
-        print("ERROR: File contents are not in correct format: ", type(error).__name__, "–", error, file=sys.stderr)
+        print("ERROR: File contents are not in correct format: ", type(error).__name__, "–", error,
+              file=sys.stderr)
         return
     update_graph(new_graph)
 
 
-openFileButton = Button(widget_frame, text="Import Graph from File", command=import_graph)
-openFileButton.grid(row=6)
-
-
-def add_edge():
-    inp = inputField.get().strip()
-    words = inp.split(" ")
-    if len(words) == 2:
-        graph.add_edge(words[0], words[1])
-    elif len(words) > 2:
-        new_str = inp.split(" ",2)[-1].replace("'", '"')
-        try:
-            attributes = json.loads(new_str)
-            graph.add_edge(words[0], words[1], **attributes)
-        except json.decoder.JSONDecodeError as error:
-            print("ERROR: Couldn't parse input as JSON", type(error).__name__, "–", error, file=sys.stderr)
+def run_algo():
+    # check step 1
+    algo_name = selectedAlgo.get()
+    if algo_name not in optionList:
+        print("ERROR: Step 1 Not Completed", file=sys.stderr)
+        return
+    # check step 2
+    if len(graph.nodes) == 0:
+        print("ERROR: Graph not added", file=sys.stderr)
+        return
+    # check step 3
+    if startNodeField.winfo_ismapped():
+        s_node = startNodeField.get()
+        if s_node not in graph.nodes:
+            print("ERROR: Start/Source Node", "'" + s_node + "'", "is not in graph",
+                  file=sys.stderr)
             return
-    update_graph(graph)
+    if sinkNodeField.winfo_ismapped():
+        e_node = sinkNodeField.get()
+        if e_node not in graph.nodes:
+            print("ERROR: Sink Nod'", "'" + e_node + "'", "is not in graph", file=sys.stderr)
+            return
+    # run algorithm
+    if algo_name == "DFS":
+        print("DFS")
+    elif algo_name == "BFS":
+        print("BFS")
+    elif algo_name == "Dijkstra's":
+        print("Dijkstra")
+    elif algo_name == "Prim's":
+        print("Prim")
+    elif algo_name == "Kruskal's":
+        print("Kruskal")
+    elif algo_name == "Ford-Fulkerson":
+        print("Ford-Fulkerson")
 
 
-cmdButton = Button(widget_frame, text="Add Edge", command=add_edge)
-cmdButton.grid(row=3, column=0, sticky=EW, padx=16)
+# Create Window
+window = Tk()
+window.title("Window")
 
-button_frame = Frame(widget_frame)
-button_frame.grid(row=5)
-
-
-def backPressed():
-    print("back")
-
-
-backButton = Button(button_frame, text="<", command=backPressed)
-backButton.grid(row=1, column=0, sticky=EW)
-
-
-def nextPressed():
-    print("next")
-
-
-nextButton = Button(button_frame, text=">", command=nextPressed)
-nextButton.grid(row=1, column=1, sticky=EW)
-
+# Create Frame
+widget_frame = Frame(window)
+widget_frame.grid(row=0, column=1, sticky=N)
+# Control Panel
+Label(widget_frame, text="Control Panel").grid(row=0, column=0, sticky=EW, pady=10)
+# Step 1 Label
+Label(widget_frame, text="Step 1: Select Algorithm").grid(row=1, column=0, sticky=EW, pady=10)
 # Menu
 selectedAlgo = tkinter.StringVar(window)
 selectedAlgo.set("Select An Algorithm")
 optionList = ["DFS", "BFS", "Dijkstra's", "Prim's", "Kruskal's", "Ford-Fulkerson"]
 questionMenu = OptionMenu(widget_frame, selectedAlgo, *optionList, command=on_algo_selection_change)
-questionMenu.grid(row=4, column=0, sticky=EW, padx=20)
-
+questionMenu.grid(row=2, column=0, sticky=EW, padx=20)
 # get selected option via selectedAlgo.get()
+# Step 2 Label
+Label(widget_frame, text="Step 2: Import Graph").grid(row=3, column=0, sticky=EW, pady=10)
+# Import Graph Button
+Label(widget_frame, text="via File").grid(row=4, column=0, sticky=EW)
+openFileButton = Button(widget_frame, text="Import Graph from File", command=import_graph)
+openFileButton.grid(row=5, column=0)
+# Edge Input
+Label(widget_frame, text="via Edge").grid(row=6, column=0, sticky=EW)
+edgeInputField = Entry(widget_frame, justify='center')
+edgeInputField.grid(row=7, column=0)
+cmdButton = Button(widget_frame, text="Add Edge", command=add_edge)
+cmdButton.grid(row=8, column=0, sticky=EW, padx=16)
+# Step 3 Label
+Label(widget_frame, text="Step 3: Input Required Info").grid(row=9, column=0, sticky=EW, pady=10)
+# Input Field(s)
+algo_required_fields_frame = Frame(widget_frame)
+algo_required_fields_frame.grid(row=10, column=0)
+startNodeLabel = Label(algo_required_fields_frame, text="Start Node")
+# startNodeLabel.grid(row=0, column=0)
+startNodeField = Entry(algo_required_fields_frame, justify='left')
+# startNodeField.grid(row=0, column=1)
+sinkNodeLabel = Label(algo_required_fields_frame, text="Sink Node")
+# sinkNodeLabel.grid(row=1, column=0)
+sinkNodeField = Entry(algo_required_fields_frame, justify='left')
+# sinkNodeField.grid(row=1, column=1)
+# Step 4 Label
+Label(widget_frame, text="Step 4: Run Algorithm").grid(row=11, column=0, sticky=EW, pady=10)
+# Run Button
+runAlgoButton = Button(widget_frame, text="Run", command=run_algo)
+runAlgoButton.grid(row=12, column=0)
+# Control Steps Buttons
+button_frame = Frame(widget_frame)
+button_frame.grid(row=13, column=0)
+backButton = Button(button_frame, text="<", command=backPressed)
+backButton.grid(row=0, column=0, sticky=EW)
+nextButton = Button(button_frame, text=">", command=nextPressed)
+nextButton.grid(row=0, column=1, sticky=EW)
 
 window.mainloop()
 
